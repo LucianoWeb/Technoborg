41c88c284fb4aada849faba4221e2249c0d0eb34
Luciano Ayala <luciano_basket94@hotmail.com>
2022-11-27 22:59:24 +0000

    Header and Navbar finished

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,43 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.png';
-import './Navbar.scss'
+import './Navbar.scss';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
+
 
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="navbar">
       <div className="navbar_links">
-          <img src={logo} alt="logo" />
-          <a href="/home">Home</a>
-          <a href="/home">What is TechnoBorg?</a>
-          <a href="#">Utilities</a>
-          <a href="#">Our Future</a>
-          <a href="#">Contact</a>
-        </div>
+        <img src={logo} alt="logo" />
+        <a href="/home">Home</a>
+        <a href="/home">What is TechnoBorg?</a>
+        <a href="#">Utilities</a>
+        <a href="#">Our Future</a>
+        <a href="#">Contact</a>
       </div>
+
+      <div className="navbar-responsive">
+
+          {toggleMenu ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+            :
+            <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+          }
+          {toggleMenu && (
+            <div className='navbar-responsive-container scale-up-center'>
+              <a href="/home">Home</a>
+              <a href="/home">What is TechnoBorg?</a>
+              <a href="#">Utilities</a>
+              <a href="#">Our Future</a>
+              <a href="#">Contact</a>
+            </div>
+          )}
+        </div>
+    </div>
   )
 }
 
